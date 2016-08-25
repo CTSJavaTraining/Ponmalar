1a72d4f6f4ec2560c6b9614f924a8415af6dc6ae
447383 <447383@PC333882.cts.com>
2016-08-25 17:15:09 +0530

    updates

--- com.training.examples/TrainingTest/src/main/java/com/training/threaddemo/ThreadLocalDemo.java
+++ com.training.examples/TrainingTest/src/main/java/com/training/threaddemo/ThreadLocalDemo.java
@@ -1,18 +1,17 @@
 package com.training.threaddemo;
 
 public class ThreadLocalDemo implements Runnable {
-
+	static Double value = 0.0D;
 	@Override
 	public void run() {
 		ThreadLocal<Double> tl = new ThreadLocal<Double>();
-		Double value = 0.0D;
 		tl.set((value = Math.random() * 100D));
 		try {
 			Thread.sleep(1000);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
-		System.out.println(Thread.currentThread().getName() + " Value: " + value);
+		System.out.println(Thread.currentThread().getName() + " Value: " + tl.get());
 	}
 
 	public static void main(String[] args) {
