39d7ddf1f782f77c9deccbe4deb13ab2d01aff9d
447383 <447383@PC333882.cts.com>
2016-09-02 13:29:12 +0530

    updates on services

---
+++ com.training.examples/EmployeeCasestudy/src/main/java/com/training/employeecasestudy/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.training.employeecasestudy;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Path("/EmployeeService")
+public class EmployeeService {
+	
+	List<Employee> employeeList = new ArrayList<Employee>();
+	EmployeeDAO ed = new EmployeeDAO();
+	
+	   @GET
+	   @Path("/users")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public void getEmployees()
+	   {
+		   ed.displayRecords();
+	   }
+}
